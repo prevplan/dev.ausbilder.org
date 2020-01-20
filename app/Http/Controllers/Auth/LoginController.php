88c0028935a6891e01edcec75c543c6f1b537f21
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2020-01-20 22:31:38 +0100

    bugfix: login bugfix (#77)
    * bugfix: login bugfix
    
    i should write tests ;-)

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,6 +44,10 @@ class LoginController extends Controller
     {
         $company = Company::find($user->last_company);
 
+        if (! $company) {
+            return false;
+        } // continue, if last company is false
+
         $active = $company->users()
             ->wherePivot('company_active', 1)
             ->wherePivot('user_active', 1)

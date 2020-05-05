db3f7b176072a6a962945fea6967f980744d7471
Holger Schmermbeck <h.schmermbeck@ausbilder.org>
2020-05-05 16:05:18 +0200

    start page changes (#305)
    start page changes for first release

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,19 +33,17 @@
             <div class="col-lg-6">
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">{{ __('Software in development') }}</h5>
 
                         <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's
-                            content.
+                            {{ __('ausbilder.org the free course management and planning software, is still in development.') }}
                         </p>
 
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
+                        <a href="https://ausbilder-forum.org" class="card-link">{{ __('More information in the forum') }}</a>
                     </div>
                 </div>
 
-                <div class="card card-primary card-outline">
+                {{-- <div class="card card-primary card-outline">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
 
@@ -56,23 +54,23 @@
                         <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a>
                     </div>
-                </div><!-- /.card -->
+                </div><!-- /.card --> --}}
             </div>
             <!-- /.col-md-6 -->
             <div class="col-lg-6">
                 <div class="card">
                     <div class="card-header">
-                        <h5 class="m-0">Featured</h5>
+                        <h5 class="m-0">{{ __('upcoming own courses') }}</h5>
                     </div>
                     <div class="card-body">
-                        <h6 class="card-title">Special title treatment</h6>
-
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text" align="center">
+                            {{ __('work in progress') }}
+                        </p>
+                        <a href="{{ route('course.overview') }}" class="card-link">{{ __('show all courses') }}</a>
                     </div>
                 </div>
 
-                <div class="card card-primary card-outline">
+                {{-- <div class="card card-primary card-outline">
                     <div class="card-header">
                         <h5 class="m-0">Featured</h5>
                     </div>
@@ -82,7 +80,7 @@
                         <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                </div>
+                </div> --}}
             </div>
             <!-- /.col-md-6 -->
         </div>
